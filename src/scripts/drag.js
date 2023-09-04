c94b9a007a1e8c9d08d79f04e3e7c80f648d171c
Sebastian Oliveto <sebastian.oliveto@hotmail.com>
2023-09-04 15:56:37 -0300

    fix drag

--- src/scripts/drag.js
+++ src/scripts/drag.js
@@ -1,6 +1,6 @@
 let miVentana = document.querySelectorAll(".miVentana");
-
-miVentana.forEach(e=>{
+let p;
+miVentana.forEach(e => {
     let isDragging = false;
     let currentX;
     let currentY;
@@ -8,42 +8,45 @@ miVentana.forEach(e=>{
     let initialY;
     let xOffset = 0;
     let yOffset = 0;
-    
+
     e.addEventListener("mousedown", dragStart);
-    e.addEventListener("mouseup", dragEnd);
-    e.addEventListener("mousemove", drag);
-    
-    function dragStart(e) {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
-        
-        if (e.target === e.currentTarget) {
+    document.addEventListener("mouseup", dragEnd);
+    document.addEventListener("mousemove", drag);
+
+    function dragStart(event) {
+        initialX = event.clientX - xOffset;
+        initialY = event.clientY - yOffset;
+
+        if (event.target === e) {
             isDragging = true;
         }
     }
-    
-    function dragEnd(e) {
+
+    function dragEnd(event) {
         initialX = currentX;
         initialY = currentY;
-        
+
         isDragging = false;
     }
-    
-    function drag(e) {
+
+    function drag(event) {
         if (isDragging) {
-            e.preventDefault();
-            
-            currentX = e.clientX - initialX;
-            currentY = e.clientY - initialY;
-            
+            event.preventDefault();
+
+            currentX = event.clientX - initialX;
+            currentY = event.clientY - initialY;
+
             xOffset = currentX;
             yOffset = currentY;
-            
-            setTranslate(currentX, currentY, e.target);
+
+            setTranslate(currentX, currentY, e);
         }
     }
-    
-    function setTranslate(xPos, yPos, el) {
-        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+
+    function setTranslate(xPos, yPos, element) {
+        element.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+        // También aplicamos la transformación al texto dentro del elemento
+        p = document.querySelector(`.${element.getAttribute("name")}`);
+        p.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
     }
 });
